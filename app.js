7e06b30cd1fca89b249abd72f82a109eddd90348
javeria-farooq <gettingeducation6@gmail.com>
2024-06-12 01:13:57 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+/*error handling: */
+
+let a = 5;
+let b = 10;
+console.log(`a = ${a}`)
+console.log(`a+b = ${a+b}`)
+console.log(`a+b = ${a+b}`)
+try{
+    console.log(`a+b = ${a+c}`)
+}
+catch(err) {
+    console.log(`error = ${err}`)
+}
+
+console.log(`a+b = ${a+b}`)
+console.log(`a+b = ${a+b}`)
+console.log(`a+b = ${a+b}`)
+console.log(`a+b = ${a+b}`)
