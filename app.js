8a5d4e8f1ea0bddea59377567fe6a0b891ffe208
javeria-farooq <gettingeducation6@gmail.com>
2024-07-28 00:54:18 +0500

    Maintain code

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-/*error handling: */
+/*//without error handling:
+let a = 5
+let b = 10
 
+console.log(`a + b = ${a + b}`)
+console.log(`a + b = ${a + b}`)
+console.log(`a + b = ${a + b}`)
+console.log(`a + b = ${a + c}`) // throws error and stop execution from here
+console.log(`a + b = ${a + b}`)
+console.log(`a + b = ${a + b}`)*/
+
+//error handling
 let a = 5;
 let b = 10;
+
 console.log(`a = ${a}`)
 console.log(`a+b = ${a+b}`)
 console.log(`a+b = ${a+b}`)
@@ -10,7 +21,10 @@ try{
 }
 catch(err) {
     console.log(`error = ${err}`)
-}
+    console.log(`error name : ${err.name}`)
+    console.log(`error message : ${err.message}`)
+    console.log(`error stack : ${err.stack}`)
+} // continuing execution
 
 console.log(`a+b = ${a+b}`)
 console.log(`a+b = ${a+b}`)
